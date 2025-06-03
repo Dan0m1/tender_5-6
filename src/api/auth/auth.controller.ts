946e783ea9f52343e065668707637f080749327b
Dan0m1 <bohdan.shevchenko182@gmail.com>
2025-06-03 12:45:46 +0300

    fix: swagger auth

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -11,20 +11,39 @@ import { GetUser } from '../../common/decorators/get-user';
 import { UserEntity } from '../../db/entities/user.entity';
 import { RegistrationCredentialsDto } from '../../common/requests/registration-credentials.dto';
 import { LocalGuard } from './guards/local.guard';
+import { TokensDto } from './types/tokens.dto';
+import {ApiBasicAuth, ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOkResponse, ApiOperation} from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Login user',
+  })
+  @ApiOkResponse({
+    description: 'The user has been successfully logged in.',
+    type: TokensDto,
+  })
+  @ApiBody({
+    type: RegistrationCredentialsDto,
+  })
   @UseGuards(LocalGuard)
   @Post('login')
-  async login(@GetUser() user: UserEntity) {
+  async login(@GetUser() user: UserEntity): Promise<TokensDto> {
     return this.authService.login(user);
   }
 
+  @ApiOperation({
+    summary: 'Register user',
+  })
+  @ApiCreatedResponse({
+    description: 'The user has been successfully registered.',
+    type: TokensDto,
+  })
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
-  async register(@Body() body: RegistrationCredentialsDto) {
-    await this.authService.register(body);
+  async register(@Body() body: RegistrationCredentialsDto): Promise<TokensDto> {
+    return this.authService.register(body);
   }
 }
--- src/api/auth/types/tokens.dto.ts
+++ src/api/auth/types/tokens.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TokensDto {
+  @ApiProperty({
+    description: 'Access token.',
+  })
   accessToken: string;
+  @ApiProperty({
+    description: 'Refresh token.',
+  })
   refreshToken: string;
 }
